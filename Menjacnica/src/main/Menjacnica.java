30e2703e95b082d6daad16f994830c1bd0c97b78
milosnikic <milos.nikic@gmail.com>
2017-04-02 15:38:10 +0200

    Implementirane sve tri metode u okviru klase Menjacnica.

--- Menjacnica/src/main/Menjacnica.java
+++ Menjacnica/src/main/Menjacnica.java
@@ -1,27 +1,47 @@
 package main;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	private LinkedList<Valuta>kursevi 
+				= new LinkedList<Valuta>();
 	@Override
 	public void dodajKurs(String naziv, String skraceniNaziv, GregorianCalendar datum, double prodajni, double srednji,
 			double kupovni) {
-		// TODO Auto-generated method stub
-
+		Valuta novi = new Valuta();
+		novi.setNaziv(naziv);
+		novi.setSkraceniNaziv(skraceniNaziv);
+		novi.setDatum(datum);
+		novi.setKupovni(kupovni);
+		novi.setProdajni(prodajni);
+		novi.setSrednji(srednji);
+		
+		kursevi.add(novi);
+		
 	}
 
 	@Override
 	public void obrisiKurs(String naziv, String skraceniNaziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+		for (int i = 0; i < kursevi.size(); i++) {
+			Valuta prolaz = kursevi.get(i);
+			if(prolaz.getNaziv().equals(naziv)	&&	 prolaz.getSkraceniNaziv().equals(skraceniNaziv)	&&	prolaz.getDatum().equals(datum)){
+				kursevi.remove(i);
+			}
+				
+		}
 	}
 
 	@Override
 	public Valuta pronadjiKurs(String naziv, String skraceniNaziv, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < kursevi.size(); i++) {
+			Valuta prolaz = kursevi.get(i);
+			if(prolaz.getNaziv().equals(naziv) && prolaz.getSkraceniNaziv().equals(skraceniNaziv) && prolaz.getDatum().equals(datum))
+				return prolaz;
+		}
 		return null;
 	}
 
