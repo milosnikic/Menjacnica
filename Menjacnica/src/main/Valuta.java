0b1087992affa183aa62fc174c447df1e6bf5b69
milosnikic <milos.nikic@gmail.com>
2017-04-02 14:55:22 +0200

    Kreirani atributi i javne get i set metode.

---
+++ Menjacnica/src/main/Valuta.java
@@ -0,0 +1,53 @@
+package main;
+
+import java.util.GregorianCalendar;
+
+public class Valuta {
+	
+	private String naziv;
+	private String skraceniNaziv;
+	private GregorianCalendar datum;
+	private double kupovni;
+	private double srednji;
+	private double prodajni;
+	
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	public double getKupovni() {
+		return kupovni;
+	}
+	public void setKupovni(double kupovni) {
+		this.kupovni = kupovni;
+	}
+	public double getSrednji() {
+		return srednji;
+	}
+	public void setSrednji(double srednji) {
+		this.srednji = srednji;
+	}
+	public double getProdajni() {
+		return prodajni;
+	}
+	public void setProdajni(double prodajni) {
+		this.prodajni = prodajni;
+	}
+	
+	
+}
